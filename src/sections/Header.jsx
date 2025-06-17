afdd57ee403b6dbda29540834d3b8e2e66e90619
aryanxengineer <aryanxengineer@gmail.com>
2025-06-17 20:44:45 +0530

    enhance header component

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -75,17 +75,18 @@ const Header = () => {
         className="absolute inset-0 z-10 pointer-events-none"
       ></div>
 
-      <h1
-        ref={nameRef}
-        className="absolute z-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-7xl leading-none font-black lg:text-[15vw]"
-      >
-        ZYNORI
-        <RedWord text="X" />
-      </h1>
-
-      <span className="absolute top-[43%] left-[8.2%] text-2xl font-bold center gap-5 z-40 lg:top-[32%] lg:left-[16%]">
-        <span>Founder of</span> <RedWord text={<FaArrowTrendUp />} />
-      </span>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 flex flex-col items-start">
+        <span className="text-2xl font-bold center gap-5 z-40 leading-none pl-1 lg:pl-3">
+          <span>Founder of</span> <RedWord text={<FaArrowTrendUp />} />
+        </span>
+        <h1
+          ref={nameRef}
+          className="z-20 text-7xl leading-none font-black lg:text-[15vw]"
+        >
+          ZYNORI
+          <RedWord text="X" />
+        </h1>
+      </div>
 
       <div className="w-full absolute top-[60%] flex flex-col items-center gap-5 lg:flex-row lg:top-[70%] justify-center">
         <button className="w-54 py-3 text-sm font-semibold rounded border-2 border-red-600 cursor-pointer">
